8704142fde5a4d0fd1bb8df513a06513a206bcb5
edudias1972 <edudias1972@gmail.com>
2022-07-27 17:24:07 -0300

    Primeiro exemplo JavaScript-DOM exemplo01

---
+++ script.js
@@ -0,0 +1,18 @@
+//acessando por id e alterando style
+
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const pagina = document.getElementById("page-title").innerText = "De volta para o futuro";
+pagina.style.color = "blue";
+
+
+
+
+/**
+ * Recupere o elemento que contém o título da página e faça algo com ele, 
+ * como alterá-lo para o nome do seu filme favorito.
+Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+Por fim, recupere o subtítulo e altere-o também.
+
+ */

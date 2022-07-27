4ed00cc4509e3c6049afef615b51d9cb23c29158
edudias1972 <edudias1972@gmail.com>
2022-07-27 18:38:37 -0300

    Alteração no arquivo scripts.js cores

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 
 const pagina = document.getElementById("page-title").innerText = "De volta para o futuro";
-pagina.style.color = "blue";
 
+const subtitulo = document.getElementById("subtitle").innerText = "De volta para o futuro II";
+
+const segundoParagrafo = document.getElementById("subtitle").innerText = "I by back segundo paragrafo";
 
 
 
 /**
  * Recupere o elemento que contém o título da página e faça algo com ele, 
  * como alterá-lo para o nome do seu filme favorito.
+ * 
 Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 Por fim, recupere o subtítulo e altere-o também.
 
